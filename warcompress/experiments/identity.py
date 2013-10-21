eede86430ffc58ce71ef43eb4cb5c6bc3d6e1514
William Mayor <mail@williammayor.co.uk>
2013-10-21 09:51:46 +0100

    Changed identity to include repetitions

--- warcompress/experiments/identity.py
+++ warcompress/experiments/identity.py
@@ -1,16 +1,23 @@
 """
-How much compression can you get on a single file?
+How much compression can you get on a single file repeated 20 times
 """
 import os
 import sys
-import shutil
 from warcompress.experiments.Experiment import Experiment
+from warcompress.encodings import utilities
 
 
-def modifier_factory(path):
+def modifier_factory(base_path, data_path):
     def g():
-        return path
-    g.dir = os.path.dirname(path)
+        with open(base_path, 'rb') as fd:
+            base = fd.read()
+            modified = base
+            for i in xrange(1, 20):
+                modified += utilities.RECORD_SEPARATOR + base
+            with open(data_path, 'wb') as fd:
+                fd.write(modified)
+            return data_path
+    g.dir = os.path.dirname(data_path)
     return g
 
 if __name__ == '__main__':
@@ -25,9 +32,8 @@ if __name__ == '__main__':
         # Directory already exists
         pass
     data_path = os.path.join(data_dir, 'data.txt')
-    shutil.copyfile(base_path, data_path)
     e = Experiment(
         'identity',
-        modifier_factory(data_path)
+        modifier_factory(base_path, data_path)
     )
     e.run(1)
