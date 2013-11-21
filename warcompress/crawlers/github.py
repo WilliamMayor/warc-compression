9baf89056973a051f195b3b2ab8b7df0efac2f58
William Mayor <mail@williammayor.co.uk>
2013-11-21 16:04:58 +0000

    Can process github crawl data

--- warcompress/crawlers/github.py
+++ warcompress/crawlers/github.py
@@ -1,14 +1,22 @@
+import re
 import sys
 import os
 import subprocess
 import time
 import datetime
+import shutil
+import socket
+import gzip
+
+from pkg_resources import resource_string
 
 import requests
+import hapy
 
 
 DEVNULL = open(os.devnull, 'wb')
 HEADERS = {'accept': 'application/vnd.github.preview'}
+HERITRIX_URL = 'https://localhost:8443'
 
 
 def clone(git_url, path):
@@ -82,14 +90,14 @@ def build_url(parts):
     return url
 
 
-def crawl(dest_dir):
+def crawl(dest_dir, from_):
     done_path = os.path.join(dest_dir, 'done.txt')
     done = load_done(done_path)
     url = {
         'base': 'https://api.github.com/search/repositories',
         'q': '.github.io',
         '_in': 'name',
-        'created': datetime.date(2008, 12, 17),
+        'created': datetime.date(from_[0], from_[1], from_[2]),
         'page': 1
     }
     while url is not None:
@@ -129,17 +137,15 @@ def crawl(dest_dir):
 
 def git_rev_list(path):
     try:
-        result = subprocess.check_output(
-            [
-                'git',
-                '--git-dir',
-                '%s/.git' % (path, ),
-                'rev-list',
-                'master'
-            ]
-        )
+        args = [
+            'git',
+            'rev-list',
+            'master'
+        ]
+        result = subprocess.check_output(args, cwd=path)
         return result.splitlines()
-    except:
+    except Exception as e:
+        raise e
         return []
 
 
@@ -147,67 +153,186 @@ def git_checkout(path, commit):
     rc = subprocess.call(
         [
             'git',
-            '--git-dir',
-            '%s/.git' % (path, ),
             'checkout',
             commit
         ],
         stdout=DEVNULL,
-        stderr=subprocess.STDOUT
+        stderr=subprocess.STDOUT,
+        cwd=path
     )
     if rc != 0:
         raise Exception('git checkout failed')
 
 
-def jekyll_serve(path):
+def jekyll_serve(path, port):
     p = subprocess.Popen(
         [
             'jekyll',
             'serve',
-            '--source',
-            path,
-            '--destination',
-            '%s/_site' % (path, )
+            '--port',
+            str(port)
         ],
         stdout=subprocess.PIPE,
-        stderr=DEVNULL
+        stderr=DEVNULL,
+        cwd=path
     )
     output = ''
     match = 'Server running'
     while output[-len(match):] != match and p.poll() is None:
         output += p.stdout.read(1)
+    if p.poll() is not None:
+        raise Exception('could not start jekyll')
     return p
 
 
-def jekyll_kill(p):
-    p.terminate()
+def wait_for(h, job_name, func_name):
+    info = h.get_job_info(job_name)
+    while func_name not in info['job']['availableActions']['value']:
+        time.sleep(1)
+        info = h.get_job_info(job_name)
 
+
+def commit_date(path, commit):
+    args = [
+        'git',
+        'show',
+        '-s',
+        '--format="%ci"',
+        commit
+    ]
+    return datetime.datetime.strptime(subprocess.check_output(args, cwd=path)[1:20],'%Y-%m-%d %H:%M:%S')
+
+
+def rewrite_warc(name, port, date):
+    h = hapy.Hapy(HERITRIX_URL, username='admin', password='admin')
+    info = h.get_job_info(name)
+    job_path = os.path.dirname(info['job']['primaryConfig'])
+    warcs_dir = os.path.join(job_path, 'latest', 'warcs')
+    for warc in os.walk(warcs_dir).next()[2]:
+        path = os.path.join(warcs_dir, warc)
+        gzr = gzip.open(path, 'r')
+        content = gzr.read()
+        gzr.close()
+        content = content.replace('http://localhost:%d' % port, 'http://%s' % name)
+        content = re.sub('^WARC-Date: .*?\n', 'WARC-Date: %s\n' % date.isoformat(), content, flags=re.MULTILINE)
+        gzw = gzip.open(path, 'w')
+        gzw.write(content)
+        gzw.close()
+
+
+def heritrix_crawl(name):
+    h = hapy.Hapy(HERITRIX_URL, username='admin', password='admin')
+    try:
+        print '        Building'
+        h.build_job(name)
+        wait_for(h, name, 'launch')
+        print '        Launching'
+        h.launch_job(name)
+        wait_for(h, name, 'unpause')
+        print '        Unpausing'
+        h.unpause_job(name)
+        print '        Waiting for finish'
+        sys.stdout.write('        ')
+        info = h.get_job_info(name)
+        finished = None
+        while finished is None:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            info = h.get_job_info(name)
+            try:
+                finished = info['job']['crawlExitStatus']
+            except:
+                time.sleep(30)
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+        print '        Tearing down'
+        h.teardown_job(name)
+        wait_for(h, name, 'build')
+    except:
+        try:
+            h.terminate_job(name)
+            wait_for(h, name, 'teardown')
+            h.teardown_job(name)
+        except:
+            pass
+
+
+def process_commit(repo_path, name, commit, port, attempts=5):
+    try:
+        print '      Checking out commit', commit
+        git_checkout(repo_path, commit)
+        print '      Starting jekyll'
+        p = jekyll_serve(repo_path, port)
+        print '      Running heritrix job'
+        heritrix_crawl(name)
+        rewrite_warc(name, port, commit_date(repo_path, commit))
+        return
+    finally:
+        try:
+            print '      Killing jekyll'
+            p.terminate()
+        except:
+            pass
+        try:
+            print '      Cleaning jekyll directory'
+            shutil.rmtree('%s/_site' % (repo_path, ))
+        except:
+            pass
+    if attempts > 0:
+        print '      Failed, trying again'
+        time.sleep(60)
+        process_commit(repo_path, name, commit, port, attempts - 1)
+
+
+def process_repo(repo_path, name):
+    print '    Creating heritrix job'
+    h = hapy.Hapy(HERITRIX_URL, username='admin', password='admin')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 0))
+    port = s.getsockname()[1]
+    s.close()
+    cxml = resource_string('warcompress', 'assets/template.cxml').replace('WC_PORT', str(port))
+    h.create_job(name)
+    h.submit_configuration(name, cxml)
+    wait_for(h, name, 'build')
+    for commit in git_rev_list(repo_path):
+        process_commit(repo_path, name, commit, port)
+    git_checkout(repo_path, 'master')
+
+                    
 
 def process(data_dir):
     print 'Processing crawled data'
+    done_path = os.path.join(data_dir, 'archived.txt')
+    done = load_done(done_path)
+    h = hapy.Hapy(HERITRIX_URL, username='admin', password='admin')
     for user in os.walk(data_dir).next()[1]:
-        print '  Processing user', user
         user_path = os.path.join(data_dir, user)
         for repo in os.walk(user_path).next()[1]:
-            print '    Processing repo', repo
-            repo_path = os.path.join(user_path, repo)
-            for commit in git_rev_list(repo_path):
-                try:
-                    print '      Checking out commit', commit
-                    git_checkout(repo_path, commit)
-                    print '      Starting jekyll'
-                    p = jekyll_serve(repo_path)
-                    #heritrix_crawl()
-                    print '      Killing jekyll'
-                    jekyll_kill(p)
-                    #jekyll_clean(repo_path)
-                except Exception as e:
-                    print '      Failed:', e
+            try:
+                name = '%s-%s' % (user, repo)
+                print '  Processing', name
+                if name in done:
+                    print '    Already archived'
+                repo_path = os.path.join(user_path, repo)
+                process_repo(repo_path, name)
+                done.add(name)
+                save_done(done_path, done)
+            except Exception as e:
+                print '    Error', str(e)
+
+
+def safe_get(l, i, d):
+    try:
+        return l[i]
+    except:
+        return d
 
 
 if __name__ == '__main__':
     data_dir = sys.argv[2]
     if sys.argv[1] == 'crawl':
-        crawl(data_dir)
+        from_ = (safe_get(sys.argv, 2, 2008), safe_get(sys.argv, 3, 12), safe_get(sys.argv, 3, 17))
+        crawl(data_dir, from_)
     elif sys.argv[1] == 'process':
         process(data_dir)
