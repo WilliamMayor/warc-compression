7982fa9bf163d280e86d26c8506c4bdf1ac8596f
William Mayor <mail@williammayor.co.uk>
2014-02-18 14:12:10 +0000

    archiver now can run in parallel on same heritrix host

--- experiments/github/archive/archive.py
+++ experiments/github/archive/archive.py
@@ -6,28 +6,44 @@ import os
 import time
 import datetime
 import sys
+import traceback
+import logging
 
 import hapy
 import sh
 
 from template import t
+from filelock import FileLock
 
 DEVNULL = open(os.devnull, 'wb')
 HERITRIX_URL = 'https://localhost:8443'
+HERITRIX = None
 
 
-def load_done(path):
+def load_list(path, need_lock=True):
+    """This is really ugly"""
     try:
-        with open(path, 'r') as fd:
-            return set(fd.read().splitlines())
+        if need_lock:
+            with FileLock(path):
+                with open(path, 'r') as fd:
+                    return set(fd.read().splitlines())
+        else:
+            with open(path, 'r') as fd:
+                return set(fd.read().splitlines())
     except:
         return set()
 
 
-def save_done(path, done):
-    with open(path, 'w') as fd:
-        fd.write('\n'.join(done))
-        fd.flush()
+def save_list(path, done, need_lock=True):
+    if need_lock:
+        with FileLock(path):
+            with open(path, 'w') as fd:
+                fd.write('\n'.join(done))
+                fd.flush()
+    else:
+        with open(path, 'w') as fd:
+            fd.write('\n'.join(done))
+            fd.flush()
 
 
 def git_rev_list(path):
@@ -50,11 +66,11 @@ def jekyll_serve(path, port):
     raise Exception('Jekyll did not start serving')
 
 
-def wait_for(h, job_name, func_name):
-    info = h.get_job_info(job_name)
+def wait_for(job_name, func_name):
+    info = HERITRIX.get_job_info(job_name)
     while func_name not in info['job']['availableActions']['value']:
         time.sleep(1)
-        info = h.get_job_info(job_name)
+        info = HERITRIX.get_job_info(job_name)
 
 
 def commit_date(path, commit):
@@ -64,12 +80,9 @@ def commit_date(path, commit):
 
 
 def rewrite_warc(name, date):
-    h = hapy.Hapy(
-        HERITRIX_URL,
-        username='admin',
-        password=sys.argv[2],
-        timeout=10.0)
-    info = h.get_job_info(name)
+    logger = logging.getLogger('archive-%s' % name)
+    logger.info('re-writing warc files')
+    info = HERITRIX.get_job_info(name)
     job_path = os.path.dirname(info['job']['primaryConfig'])
     warcs_dir = os.path.join(job_path, 'latest', 'warcs')
     for warc in os.walk(warcs_dir).next()[2]:
@@ -88,114 +101,126 @@ def rewrite_warc(name, date):
 
 
 def heritrix_crawl(name):
-    h = hapy.Hapy(
-        HERITRIX_URL,
-        username='admin',
-        password=sys.argv[2],
-        timeout=10.0)
+    logger = logging.getLogger('archive-%s' % name)
     try:
-        print '        Building'
-        h.build_job(name)
-        wait_for(h, name, 'launch')
-        print '        Launching'
-        h.launch_job(name)
-        wait_for(h, name, 'unpause')
-        print '        Unpausing'
-        h.unpause_job(name)
-        print '        Waiting for finish'
-        sys.stdout.write('        ')
-        info = h.get_job_info(name)
+        logger.info('Building')
+        HERITRIX.build_job(name)
+        wait_for(name, 'launch')
+        logger.info('Launching')
+        HERITRIX.launch_job(name)
+        wait_for(name, 'unpause')
+        logger.info('Unpausing')
+        HERITRIX.unpause_job(name)
+        logger.info('Waiting for finish')
+        info = HERITRIX.get_job_info(name)
         finished = None
         while finished is None:
-            sys.stdout.write('.')
-            sys.stdout.flush()
-            info = h.get_job_info(name)
+            logger.info('Not finished')
+            info = HERITRIX.get_job_info(name)
             try:
                 finished = info['job']['crawlExitStatus']
             except:
                 time.sleep(30)
-        sys.stdout.write('\n')
-        sys.stdout.flush()
-        print '        Tearing down'
-        h.teardown_job(name)
-        wait_for(h, name, 'build')
+        logger.info('Tearing down')
+        HERITRIX.teardown_job(name)
+        wait_for(name, 'build')
     except:
         try:
-            h.terminate_job(name)
-            wait_for(h, name, 'teardown')
-            h.teardown_job(name)
+            HERITRIX.terminate_job(name)
+            wait_for(name, 'teardown')
+            HERITRIX.teardown_job(name)
         except:
             pass
 
 
-def process_commit(repo_path, name, commit, port, attempts=5):
+def process_commit(repo_path, name, commit, port):
+    logger = logging.getLogger('archive-%s' % name)
     try:
-        print '      Checking out commit', commit
+        logger.info('Checking out commit: %s' % commit)
         git_checkout(repo_path, commit)
-        print '      Starting jekyll'
+        logger.info('Starting jekyll')
         p = jekyll_serve(repo_path, port)
-        print '      Running heritrix job'
+        logger.info('Running heritrix job')
         heritrix_crawl(name)
         rewrite_warc(name, commit_date(repo_path, commit))
         return
+    except:
+        pass
     finally:
         try:
-            print '      Killing jekyll'
+            logger.info('Killing jekyll')
             p.terminate()
         except:
             pass
         try:
-            print '      Cleaning jekyll directory'
+            logger.info('Cleaning jekyll directory')
             shutil.rmtree('%s/_site' % (repo_path, ))
         except:
             pass
-    if attempts > 0:
-        print '      Failed, trying again'
-        time.sleep(60)
-        process_commit(repo_path, name, commit, port, attempts - 1)
 
 
 def process_repo(repo_path, name):
-    print '    Creating heritrix job'
-    h = hapy.Hapy(
+    logger = logging.getLogger('archive-%s' % name)
+    rev_list = git_rev_list(repo_path)
+    if len(rev_list) > 0:
+        logger.info('Creating heritrix job')
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', 0))
+        port = s.getsockname()[1]
+        s.close()
+        cxml = t.replace('WC_PORT', str(port))
+        HERITRIX.create_job(name)
+        HERITRIX.submit_configuration(name, cxml)
+        wait_for(name, 'build')
+        for commit in rev_list:
+            process_commit(repo_path, name, commit, port)
+        git_checkout(repo_path, 'master')
+        return True
+    else:
+        logger.info('No commits')
+        return False
+
+
+def find_job(data_dir, done, current):
+    for user in os.walk(data_dir).next()[1]:
+        user_path = os.path.join(data_dir, user)
+        for repo in os.walk(user_path).next()[1]:
+            name = '%s-%s' % (user, repo)
+            if name in done or name in current:
+                continue
+            path = os.path.join(user_path, repo)
+            return name, path
+    return None, None
+
+
+def process(data_dir):
+    done_path = os.path.join(data_dir, 'archived.txt')
+    done = load_list(done_path)
+    current_path = os.path.join(data_dir, 'current.txt')
+    with FileLock(current_path):
+        current = load_list(current_path, need_lock=False)
+        job, repo_path = find_job(data_dir, done, current)
+        current.add(job)
+        save_list(current_path, current, need_lock=False)
+    if None not in [job, repo_path]:
+        logger = logging.getLogger('archive-%s' % job)
+        hdlr = logging.FileHandler('archive-%s.log' % job)
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        hdlr.setFormatter(formatter)
+        logger.addHandler(hdlr)
+        logger.setLevel(logging.INFO)
+        try:
+            process_repo(repo_path, job)
+            done.add(job)
+            save_list(done_path, done)
+        except:
+            logger.error('Error occurred')
+            logger.error(traceback.format_exc())
+
+if __name__ == '__main__':
+    HERITRIX = hapy.Hapy(
         HERITRIX_URL,
         username='admin',
         password=sys.argv[2],
         timeout=10.0)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 0))
-    port = s.getsockname()[1]
-    s.close()
-    cxml = t.replace('WC_PORT', str(port))
-    h.create_job(name)
-    h.submit_configuration(name, cxml)
-    wait_for(h, name, 'build')
-    for commit in git_rev_list(repo_path):
-        process_commit(repo_path, name, commit, port)
-    git_checkout(repo_path, 'master')
-
-
-def process(data_dir):
-    print 'Processing crawled data'
-    done_path = os.path.join(data_dir, 'archived.txt')
-    done = load_done(done_path)
-    for user in os.walk(data_dir).next()[1]:
-        user_path = os.path.join(data_dir, user)
-        for repo in os.walk(user_path).next()[1]:
-            try:
-                name = '%s-%s' % (user, repo)
-                print '  Processing', name
-                if name in done:
-                    print '    Already archived'
-                    continue
-                repo_path = os.path.join(user_path, repo)
-                process_repo(repo_path, name)
-                done.add(name)
-                save_done(done_path, done)
-                return
-            except Exception as e:
-                print '    Error', str(e)
-
-if __name__ == '__main__':
-    print sys.argv[2]
     process(sys.argv[1])
---
+++ experiments/github/archive/process.sh
@@ -0,0 +1,26 @@
+CODE_DIR="/Users/william/Projects/warc-compression/experiments/"
+HERITRIX_DIR="/usr/local/heritrix-3.1.1/"
+HERITRIX_PASSWORD="8700mm0n9wnxkjc1cvtbdhdqyxlji7z0"
+CRAWL_DIR="/Users/william/Desktop/test_data/crawl/"
+
+. "${CODE_DIR}venv/bin/activate"
+export JAVA_OPTS=" -Xmx1024m"
+kill -9 $(cat "${HERITRIX_DIR}heritrix.pid")
+"${HERITRIX_DIR}bin/heritrix" --web-admin admin:"${HERITRIX_PASSWORD}" --web-bind-hosts 0.0.0.0
+
+while [ ! -f "${CODE_DIR}process_stop" ]
+do
+    while [[ $( jobs | wc -l ) -ge 10 ]]
+    do
+        if [ -f "${CODE_DIR}process_stop" ]
+        then
+            echo "Stopping, waiting for archivers: $(date)"
+            wait
+            exit
+        fi
+        echo "No empty processes: $(date)"
+        sleep 300
+    done
+    echo "Spawning new archiver"
+    python -u "${CODE_DIR}github/archive/archive.py" "${CRAWL_DIR}" "${HERITRIX_PASSWORD}" &
+done
