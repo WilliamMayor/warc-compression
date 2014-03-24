290ca3851f433ccf20919d7737d483d37af923aa
William Mayor <mail@williammayor.co.uk>
2014-03-24 11:37:50 +0000

    fixed imports

--- experiments/github/archive/organise.py
+++ experiments/github/archive/organise.py
@@ -1,6 +1,7 @@
 import os
 
 import archive
+from github.WARC import WARC
 
 jobs_dir = '/cs/research/fmedia/data5/wmayor/github/heritrix-3.1.1/jobs'
 warcs_dir = '/cs/research/fmedia/data5/wmayor/github/warcs'
@@ -9,7 +10,7 @@ for e in os.listdir(jobs_dir):
     p = os.path.join(jobs_dir, e)
     if os.path.isdir(p):
         print p
-        for r in filter_records(p):
+        for r in archive.filter_records(p):
             date, time = r.headers['WARC-Date'].split('T', 1)
             w = WARC(os.path.join(warcs_dir, date + ".warc"), order_by='WARC-Date')
             w.add(r)
